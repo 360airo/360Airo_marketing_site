b96384caf600a35648fdf921a826146b921f8e55
Madhu0614 <globopersona81@gmail.com>
2026-07-20 23:23:00 +0530

    UPDATE

--- next.config.js
+++ next.config.js
@@ -8,6 +8,21 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.360airo.com',
+          },
+        ],
+        destination: 'https://360airo.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
